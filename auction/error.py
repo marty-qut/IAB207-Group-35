1b492173bedebdf0098cc56685fa844f0c93f7a4
Zelo <46229265+ZeloAvarosa@users.noreply.github.com>
2020-10-29 18:30:22 +1000

    moved errors to new folders

--- auction/error.py
+++ auction/error.py
@@ -4,9 +4,28 @@ bp = Blueprint('error', __name__)
 
 # app name
 @app.errorhandler(404)
-
 # inbuilt function which takes error as parameter
 def not_found(e):
-
 # defining function
   return render_template("404.html")
+
+# app name
+@app.errorhandler(410)
+# inbuilt function which takes error as parameter
+def not_found(e):
+# defining function
+  return render_template("410.html")
+
+# app name
+@app.errorhandler(403)
+# inbuilt function which takes error as parameter
+def Forbidden(e):
+# defining function
+  return render_template("403.html")
+
+# app name
+@app.errorhandler(500)
+# inbuilt function which takes error as parameter
+def errorhandler(e):
+# defining function
+  return render_template("500.html")
