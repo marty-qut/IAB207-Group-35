f268c944057442b5a0e209a225d99dbb80c5159f
marty-qut <n10589775@qut.edu.au>
2020-11-01 17:24:48 +1000

    Change db reference to Postgres

--- auction/__init__.py
+++ auction/__init__.py
@@ -15,7 +15,7 @@ def create_app():
     app.debug=True
     app.secret_key='utroutoru'
     #set the app configuration data
-    app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///marketplace.sqlite'
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
     #initialize db with flask app
     db.init_app(app)
 
